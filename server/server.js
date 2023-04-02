bb54ba253301d36115587c3c414185c05cf8472b
rahim <umudyan2014@gmail.com>
2023-04-02 20:16:50 +0300

    deploying

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import cors from "cors";
 
 const configuration = new Configuration({
   organization: "org-uhp3by1J9MPnkHVH8MxRRoTR",
-  apiKey: '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',
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 const app = express();
